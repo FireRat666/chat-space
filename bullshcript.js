4a126858599f9e1d8a67a41132f48bc77c3b7bba
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-12-08 22:40:31 +1100

    update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -5,7 +5,7 @@ if(window.isBanter){
   async function somerandomStartCrap() {
     const waitingForUnity = async () => { while (!chatscene.unityLoaded) { await new Promise(resolve => setTimeout(resolve, 500)); } };
     await waitingForUnity(); console.log("SCRIPT: Unity-Loaded");
-    setTimeout(() => { loadSettings(); infotextStuff(); landingPlatform(); }, 1000);
+    setTimeout(() => { loadSettings(); infotextStuff(); landingPlatform(); drawingTools(); }, 1000);
   };
 
   function loadSettings() {
@@ -52,6 +52,23 @@ if(window.isBanter){
     plane20transform.localPosition = new BS.Vector3(0,-0.2,0);
     plane20transform.localScale = new BS.Vector3(10,0.05,10);
   }
+
+  async function drawingTools() {
+    const kitDrawWindows = "https://chat-space.firer.at/DrawTools/kitbundle_standalonewindows.banter";
+    const kitDrawAndroid = "https://chat-space.firer.at/DrawTools/kitbundle_android.banter";
+    const drawToolsObject = await new BS.GameObject("MyDrawTools");
+    const kitDrawBundle = await drawToolsObject.AddComponent(new BS.BanterAssetBundle(kitDrawWindows, null, null, kitDrawAndroid));
+    setTimeout(async () => {
+      const drawToolItemPath = "assets/_prefabs/drawgadget/drawtool.prefab";
+      const drawToolGameObject = await new BS.GameObject("MyKitItem");
+      const drawToolItem = await drawToolGameObject.AddComponent(new BS.BanterKitItem(drawToolItemPath));
+      const drawToolGameObjectTransform = await drawToolGameObject.AddComponent(new BS.Transform());
+      setTimeout(async () => {
+        drawToolGameObjectTransform.position = new BS.Vector3(4,1.3,-2);
+        drawToolGameObjectTransform.eulerAngles = new BS.Vector3(0,-90,0);
+      }, 500);
+    }, 1000);
+  };
 
   somerandomStartCrap();
 
